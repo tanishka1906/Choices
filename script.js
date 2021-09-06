c19fc56b38476381e7222f0aaef9f0afd5d45a11
wildcat198 <tanishkagr8a@gmail.com>
2021-09-06 18:55:23 +0530

    any msg

---
+++ script.js
@@ -0,0 +1,6 @@
+function wc(){
+    document.getElementById("banner").style.backgroundImage = "url(https://images6.alphacoders.com/106/thumb-1920-1065654.png)"
+  }
+  function a(){
+    document.getElementById("banner").style.backgroundImage = "url(https://wallpapercave.com/wp/wp1858907.png)"
+  }
